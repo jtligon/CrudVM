37fbb85c383c82cd648374e8dd928ed96e6fed73
Jeff Ligon <jligon@redhat.com>
2020-11-24 22:03:07 -0500

    Initial Commit

---
+++ CrudVM/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  CrudVM
+//
+//  Created by Jeff Ligon on 11/24/20.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
